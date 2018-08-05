7f9d4e5545ddc888536a4d13ab750e1d26d33731
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-08-05 21:10:23 +0200

    Pull issues from Github

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,29 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args()
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 
   @doc """
