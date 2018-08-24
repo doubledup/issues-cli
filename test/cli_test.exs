a09716e7ed168d85e0e0a7c691d8ad9efec18b98
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-08-24 08:56:55 +0200

    Use imports in tests

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,22 +2,25 @@ defmodule CLITest do
   use ExUnit.Case
   doctest Issues.CLI
 
+  import Issues.CLI, only: [parse_args: 1,
+                            sort_into_descending_order: 1]
+
   test "with no arguments, -h or --help, return :help" do
-    assert Issues.CLI.parse_args([]) == :help
-    assert Issues.CLI.parse_args(["-h", "anything"]) == :help
-    assert Issues.CLI.parse_args(["-help", "anything"]) == :help
+    assert parse_args([]) == :help
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["-help", "anything"]) == :help
   end
 
   test "with two strings, use the default count value" do
-    assert Issues.CLI.parse_args(["uname", "projname"]) == {"uname", "projname", 4}
+    assert parse_args(["uname", "projname"]) == {"uname", "projname", 4}
   end
 
   test "with three strings, return their parsed values" do
-    assert Issues.CLI.parse_args(["uname", "projname", "42"]) == {"uname", "projname", 42}
+    assert parse_args(["uname", "projname", "42"]) == {"uname", "projname", 42}
   end
 
   test "sort_descending orders the correct way" do
-    result = Issues.CLI.sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
     created_at_values = Enum.map(result, &(Map.get(&1, "created_at")))
     assert created_at_values == ~w{c b a}
   end
