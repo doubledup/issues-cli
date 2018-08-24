f8ed2a499583a3fd1569a49c1227c6df948a2ec3
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-08-24 08:51:38 +0200

    Sort issues into descending order

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -15,4 +15,14 @@ defmodule CLITest do
   test "with three strings, return their parsed values" do
     assert Issues.CLI.parse_args(["uname", "projname", "42"]) == {"uname", "projname", 42}
   end
+
+  test "sort_descending orders the correct way" do
+    result = Issues.CLI.sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    created_at_values = Enum.map(result, &(Map.get(&1, "created_at")))
+    assert created_at_values == ~w{c b a}
+  end
+
+  defp fake_created_at_list(values) do
+    Enum.map(values, &(%{"created_at" => &1, "other_data" => 42}))
+  end
 end
