dc6c66ff54a2c37d6fc735fb8bba0ad6052c92ec
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-11-18 15:00:14 +0200

    Move table formatting into its own module

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [format_for_columns: 2]
+
   @default_count 4
 
   @module_doc """
@@ -71,56 +73,5 @@ defmodule Issues.CLI do
   def sort_into_descending_order(list_of_issues) do
     list_of_issues
     |> Enum.sort(&(&1["created_at"] >= &2["created_at"]))
-  end
-
-  def format_for_columns(list_of_issues, headings) do
-    column_widths =
-      for heading <- headings do
-        max_length_of_field(list_of_issues, heading)
-      end
-
-    heading = render_row(headings, column_widths)
-
-    separator =
-      column_widths
-      |> Enum.map(&String.duplicate("-", &1))
-      |> Enum.join("-+-")
-
-    rows =
-      list_of_issues
-      |> extract_issue_data(headings)
-      |> Enum.map(&(render_row(&1, column_widths)))
-      |> Enum.join("\n")
-
-    Enum.join([heading, separator, rows], "\n")
-  end
-
-  defp max_length_of_field(list_of_maps, field) do
-    list_of_maps
-    |> Enum.map(
-      &(&1[field]
-        |> Kernel.to_string
-        |> String.length)
-    )
-    |> Enum.max
-    |> Kernel.max(String.length(field))
-  end
-
-  defp render_row(fields, column_widths) do
-    Enum.zip(fields, column_widths)
-    |> Enum.map(fn {field, width} ->
-      field
-      |> Kernel.to_string
-      |> String.pad_trailing(width)
-    end)
-    |> Enum.join(" | ")
-  end
-
-  defp extract_issue_data(maps, headings) do
-    for map <- maps do
-      for heading <- headings do
-        map[heading]
-      end
-    end
   end
 end
